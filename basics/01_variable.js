93fae030ce6ab6cc930881eb6e98b8ec3bc8bf52
VIDHI VIJAY MISHRA <vidhimishra211205@gmail.com>
2026-07-17 09:42:43 +0000

    2

--- basics/01_variable.js
+++ basics/01_variable.js
@@ -7,3 +7,11 @@ userCity="chandrapur"
 //userId =2112  // not allowed to use const then change value
 console.log(userId);
 console.table([userName, userEmail, userCity,accState] );
+
+
+//numm is a standalone statement and can be used anywhere in the code.
+
+//symbol is a unique and immutable primitive value and may be used as the key of an Object property.
+
+console.log(typeof age);
+console.log(typeof n)
---
+++ basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" ; //treats all JS code as newer version of JS
+//alert("Hello World") 
+ // we are using node js not browser so alert will not work  
+
+
+ console.log(3+3)  
+ let name = "Vidhi"
+ let age = 20
+ let isMarried = false
+ let accState; //undefined
+ let accCity = null //null
+ let user = {id:1234, name:"Vidhi", email:"vidhimishra@gmail.com"}
+ //number, string, boolean, undefined, null, object, symbol
+ //numm is a standalone statement and can be used anywhere in the code.
+
+//symbol is a unique and immutable primitive value and may be used as the key of an Object property.
+console.log(typeof age);
+console.log(typeof accCity);
+
+
+//INETRVIEW QUESTIONS NULL IS AN OBJECT AND UNDEFINED IS A TYPE OF VARIABLE WHICH IS NOT ASSIGNED ANY VALUE.
