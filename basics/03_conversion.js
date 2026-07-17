ee0b5911c652f1d260e49ec248969277b2e27034
VIDHI VIJAY MISHRA <vidhimishra211205@gmail.com>
2026-07-17 10:14:44 +0000

    3

---
+++ basics/03_conversion.js
@@ -0,0 +1,29 @@
+//let score =null
+//let score = "vidhi";  // NaN occurs when we try to convert a string into number which is not a number.
+//let score ="true";
+console.log(typeof (score));
+console.log(typeof score);
+let value=Number(score)
+console.log(typeof value);
+console.log(value);
+
+
+//let loggedIn = 1
+let loggedInValue = Boolean(loggedIn)
+console.log(typeof loggedInValue);
+console.log(loggedInValue);
+
+//NaN is a special value in JavaScript that stands for "Not-a-Number". It is used to represent a value that is not a valid number. NaN is typically the result of an operation that cannot produce a meaningful numeric result, such as dividing zero by zero or attempting to convert a non-numeric string to a number. In JavaScript, NaN is considered a falsy value, meaning it evaluates to false in boolean contexts.
+
+
+//1=> true 0=> false
+//"" empty string => false
+// "vidhi" => true
+
+//*********************OPERATIONS **********
+
+
+let value = 3
+let negativeValue = -value
+
+console.log(negativeValue);
