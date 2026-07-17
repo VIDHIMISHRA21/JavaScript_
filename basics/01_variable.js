7d43587a3440b3a45bc3abbac918fec6aa0798cf
VIDHI VIJAY MISHRA <vidhimishra211205@gmail.com>
2026-07-17 09:15:41 +0000

    step1: variable

---
+++ basics/01_variable.js
@@ -0,0 +1,9 @@
+const userId =1234
+let userName = "Vidhi"
+var userEmail = "vidhimishra@gmail.com"
+let accState; 
+userCity="chandrapur"
+
+//userId =2112  // not allowed to use const then change value
+console.log(userId);
+console.table([userName, userEmail, userCity,accState] );
