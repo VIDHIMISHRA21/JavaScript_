ad6568faa90097568e5106ddf7181133156d6342
VIDHI VIJAY MISHRA <vidhimishra211205@gmail.com>
2026-07-18 12:05:53 +0000

    04_

--- basics/04_datatypes.js
+++ basics/04_datatypes.js
@@ -16,3 +16,9 @@ function greet() {
 }
 
 //objects are mutable, primitive data types are immutable
+ 
+// ******************************STACK & HEAP MEMORY***********************
+
+// stack(primitive datatypes) - stores data in a linear fashion, faster access, limited size
+
+// heap(reference datatypes) - stores data in a non-linear fashion, slower access, larger size
