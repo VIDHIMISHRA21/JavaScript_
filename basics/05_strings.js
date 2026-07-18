80d5ff6140e65f31980f66c50275e3416d46c00b
VIDHI VIJAY MISHRA <vidhimishra211205@gmail.com>
2026-07-18 12:30:10 +0000

    5

---
+++ basics/05_strings.js
@@ -0,0 +1,32 @@
+//strings  
+const name ="VIDHI";
+const surname="MISHRA";
+//console.log(name + surname); //VIDHIMISHRA
+
+console.log(`LORD KRISN LOVES ${name} and RADHA RANI ALSO ${surname}`);
+
+
+const job=new String('Software Engineer');
+console.log(job[2]);
+console.log(job.length);
+console.log(job.__proto__);
+console.log(job.toUpperCase());
+console.log(job.charAt(5));
+console.log(job.indexOf('Engineer'));
+
+//stings methods
+const str="Hello, I am Vidhi Mishra";
+console.log(str.split(' '));
+console.log(str.replace('Vidhi','Rohit'));
+console.log(str.includes('Vidhi'));
+console.log(str.startsWith('Hello'));
+console.log(str.endsWith('Mishra'));    
+//slice method
+const str1="Hello, I am Vidhi Mishra";
+console.log(str1.slice(0,5));    
+
+
+//split method
+const str2="Hello, I am Vidhi Mishra";
+console.log(str2.split(' '));
+
