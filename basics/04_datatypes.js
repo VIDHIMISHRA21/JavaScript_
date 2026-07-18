bab02a845854201dfae4db1522a02a46d5e877a0
VIDHI VIJAY MISHRA <vidhimishra211205@gmail.com>
2026-07-18 11:58:06 +0000

    04

---
+++ basics/04_datatypes.js
@@ -0,0 +1,18 @@
+// primitive data types
+let name = "John"; // string
+let age = 30; // number
+let isStudent = true; // boolean
+let address = null; // null
+let phoneNumber; // undefined
+// symbol
+let uniqueId = Symbol("id"); // symbol
+
+// reference data types(non primitive type )
+let hobbies = ["reading", "swimming"]; // array
+let person = { name: "John", age: 30 }; // object   
+//function
+function greet() {
+  console.log("Hello!");
+}
+
+//objects are mutable, primitive data types are immutable
