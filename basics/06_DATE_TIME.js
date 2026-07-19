87b535fbce9628491c79432f3fd0d939e884efc1
VIDHI VIJAY MISHRA <vidhimishra211205@gmail.com>
2026-07-19 07:47:23 +0000

    6

---
+++ basics/06_DATE_TIME.js
@@ -0,0 +1,20 @@
+// Date and Time in JavaScript
+//date object is used to work with dates and times in JavaScript. It provides various methods to get and set date and time values, as well as to perform calculations with dates.
+
+let currentDate = new Date();
+// console.log(currentDate);
+
+// console.log(currentDate.toString());
+
+// console.log(currentDate.toLocaleString());
+
+
+console.log(typeof currentDate);
+
+let myDate =new Date(2005,21); // date year, month, day
+console.log(myDate.toDateString());
+
+
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
+
